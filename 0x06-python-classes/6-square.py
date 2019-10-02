4fb7e3ad419e9b5e8475da918e1cc9e2f4d04fb6
Banu Sapakova <banuaksom@gmail.com>
2019-10-02 04:43:54 +0000

    added

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+
+    def area(self):
+        return self.__size**2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        self.__position = value
+        if type(value) != tuple and len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for m in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for n in range(self.__position[0]):
+                    print(' ', end='')
+                for j in range(self.__size):
+                    print('#', end='')
+                print()
