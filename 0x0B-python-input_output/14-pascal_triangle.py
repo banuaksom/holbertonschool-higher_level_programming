d60cb92e7572bf48c4bece7936cec02575a4b4a6
Banu Sapakova <banuaksom@gmail.com>
2019-10-17 07:14:52 +0000

    added factorial method

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-import math
-
-
-def factorial(i, j):
-    return math.factorial(i) // (math.factorial(j) * math.factorial(i - j))
-
-
 def pascal_triangle(n):
     triangle = []
     for i in range(n):
         row = []
         for j in range(i + 1):
-            row.append(factorial(i, j))
+            row.append(factorial(i) // (factorial(j) * factorial(i - j)))
         triangle.append(row)
     return triangle
+
+
+def factorial(num):
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i
+    return factorial
