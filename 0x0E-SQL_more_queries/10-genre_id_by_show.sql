9cc2b74fbb82b57d54356e5f763c648b1128032f
Banu Sapakova <banuaksom@gmail.com>
2019-11-27 04:38:23 +0000

    added

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-select tv_shows.title, tv_show_genres.genre_id from tv_shows join tv_show_genres
-on tv_show_genres.show_id = tv_shows.id order by 1, 2;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows join tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id ORDER BY 1, 2;
