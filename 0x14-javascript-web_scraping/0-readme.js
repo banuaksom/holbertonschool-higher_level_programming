ed39151cda4d251d0eb6b26e4f4ea9214b786a90
Banu Sapakova <banuaksom@gmail.com>
2020-02-11 20:08:08 +0000

    first commit

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* reads and prints the content of a file */
+const fs = require('fs');
+
+fs.readFile(process.argv[2], (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data.toString().replace(/\r?\n|\r/g, ' '));
+});
