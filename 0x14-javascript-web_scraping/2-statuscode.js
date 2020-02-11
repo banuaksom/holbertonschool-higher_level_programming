9196a3e21f5efc97f272cda4b1ea748f6027ea2a
Banu Sapakova <banuaksom@gmail.com>
2020-02-11 21:03:27 +0000

    semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 /* displays the status code of a GET request */
 const request = require('request');
 
-request.get(process.argv[2]).on('response', function(response) {
+request.get(process.argv[2]).on('response', function (response) {
   console.log('code:', response.statusCode);
 });
