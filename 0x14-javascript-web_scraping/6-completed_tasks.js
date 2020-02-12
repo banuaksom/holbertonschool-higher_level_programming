ac97c23cf0869454a6298c7159d754c976f55192
Banu Sapakova <banuaksom@gmail.com>
2020-02-12 06:04:15 +0000

    first commit

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+
+const dicti = {};
+
+request.get(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  for (const task of JSON.parse(body)) {
+      if (task.completed) {
+          if (dicti[task.userId]) {
+             dicti[task.userId]++;
+          } else {
+            dicti[task.userId] = 1;
+          }
+      }
+  }
+  console.log(dicti);
+});
