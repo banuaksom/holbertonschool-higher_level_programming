57b18213abe32b166824dc8778f7efb45050b0bf
Banu Sapakova <banuaksom@gmail.com>
2020-02-12 06:05:30 +0000

    req check fixed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,13 +8,13 @@ request.get(process.argv[2], function (error, response, body) {
     console.log(error);
   }
   for (const task of JSON.parse(body)) {
-      if (task.completed) {
-          if (dicti[task.userId]) {
-             dicti[task.userId]++;
-          } else {
-            dicti[task.userId] = 1;
-          }
+    if (task.completed) {
+      if (dicti[task.userId]) {
+        dicti[task.userId]++;
+      } else {
+        dicti[task.userId] = 1;
       }
+    }
   }
   console.log(dicti);
 });
